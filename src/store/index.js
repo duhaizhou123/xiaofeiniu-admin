5e3c9f370b6d5ebc9472d65b9e862302657e4e0d
unknown <942216863@qq.com>
2020-06-10 23:12:34 +0800

    完成store.js中相关配置

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    adminName: '',
+    globalSettings: {
+      apiUrl: '127.0.0.1:8090'
+    }
   },
   mutations: {
+    setAdminName(state,value){
+      state.adminName = value
+    },
+    setGlobalSettings(state,value){
+      state.apiUrl = value
+    }
   },
   actions: {
   },
